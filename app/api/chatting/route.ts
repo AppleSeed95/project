2a6a9a2c1917427560ac7e6f8f545efbcc973446
Kyosuke <everlastingfriendship194@gmail.com>
2024-02-08 08:03:02 +0900

    fix

--- app/api/chatting/route.ts
+++ app/api/chatting/route.ts
@@ -16,7 +16,7 @@ export async function POST(request: NextRequest) {
     });
     await executeQuery(`
       CREATE TABLE IF NOT EXISTS message (
-        id INT AUTO_INCREMENT PRIMARY KEY, 
+        id INT AUTO_INCREMENT PRIMARY KEY, ds
         roomId int,
         userId int,        
         day VARCHAR(255),
